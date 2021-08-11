7455ea765a57f28a21807f58de6c0210829ddda4
serverLesson123 <88795740+serverLesson123@users.noreply.github.com>
2021-08-11 21:14:07 +0300

    Added line before all

--- partOfJava/src/lesson4/Main.java
+++ partOfJava/src/lesson4/Main.java
@@ -2,6 +2,8 @@ package lesson4;
 
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Hello world123");
         System.out.println("Hello world");
         System.out.println("Added new line");
     }
